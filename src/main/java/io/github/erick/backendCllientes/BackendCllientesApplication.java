33f633c431db021db96242b9a293ffac72d6b5f1
£rick Oliveira <erickoliveira66@gmail.com>
2020-11-13 10:47:30 -0300

    Criando usuario admin

--- src/main/java/io/github/erick/backendCllientes/BackendCllientesApplication.java
+++ src/main/java/io/github/erick/backendCllientes/BackendCllientesApplication.java
@@ -2,7 +2,9 @@ package io.github.erick.backendCllientes;
 
 
 import io.github.erick.backendCllientes.model.entity.Cliente;
+import io.github.erick.backendCllientes.model.entity.Usuario;
 import io.github.erick.backendCllientes.model.repository.ClienteRepository;
+import io.github.erick.backendCllientes.model.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +13,23 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendCllientesApplication {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Bean
+	public CommandLineRunner init(){
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				Usuario usuario = new Usuario();
+				usuario.setUsername("Erick");
+				usuario.setNomeCompleto("Erick Oliveira Santos");
+				usuario.setPassword("admin");
+				usuarioRepository.save(usuario);
+			}
+		};
+	}
 
 	@Bean
 	public CommandLineRunner run(@Autowired ClienteRepository repository) {
